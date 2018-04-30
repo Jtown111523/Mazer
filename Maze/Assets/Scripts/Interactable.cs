b49a07201f0137a4a48f05de8eddd78bd1a2333f
Jack <jjhaek@cox.net>
2018-04-30 13:27:14 -0700

    Added Map and Interactable

---
+++ Maze/Assets/Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius;
+
+    bool inTrigger;
+
+    SphereCollider sphere;
+
+    private void Awake()
+    {
+        sphere = GetComponent<SphereCollider>();
+        sphere.radius = radius;
+        sphere.isTrigger = true;
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //display UI for player
+            //Debug.Log("Can Interact");
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                //if player presses "F" they pick up the Item
+                Debug.Log("Picked Up Item");
+
+            }
+        }
+
+    }
+
+
+}
---
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    CharacterController characterController;
+    public float runspeed = 6.0F;
+    public float walkspeed = 3.0F;
+    float speed;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runspeed;
+        }
+        else
+        {
+            speed = walkspeed;
+        }
+      
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+}
+
+}
